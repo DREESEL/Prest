512ab57f410f931d414cbb90b1588cb220426d87
Lynnen04 <55860738+DREESEL@users.noreply.github.com>
2022-05-31 19:55:04 -0500

    Update router.jsx

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,19 +2,21 @@ import React from 'react';
 import SignInScreen from '../screens/signIn';
 import SignUpScreen from '../screens/signUp';
 import ForgotPwdScreen from '../screens/forgotPwd';
-/*import HomeScreen from '../screens/home'; */
-/*import FavouritesScreen from '../screens/favourites';*/
-/*import HistoryScreen from '../screens/history';*/
-/*import CartScreen from '../screens/cart';*/
-/*import AccountScreen from '../screens/account';*/
-/*import Drawer from '../components/drawer'; */
+import HomeScreen from '../screens/home'; 
+import FavouritesScreen from '../screens/favourites';
+import HistoryScreen from '../screens/history';
+import CartScreen from '../screens/cart';
+import AccountScreen from '../screens/account';
+import PrevistaScreen from '../screens/prevista';
+import Drawer from '../components/drawer'; 
 import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer /*, createSwitchNavigator */ } from 'react-navigation';
-import { COLORS /*, SIZES */ } from '../constants/theme';
-/*import { createBottomTabNavigator } from 'react-navigation-tabs';*/
-/*import Icon from 'react-native-vector-icons/MaterialCommunityIcons';*/
-/*import Icon2 from 'react-native-vector-icons/Ionicons';*/
-/*import { createDrawerNavigator } from 'react-navigation-drawer';*/
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { COLORS , SIZES } from '../constants/theme';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Icon2 from 'react-native-vector-icons/Ionicons';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+
 
 const authStack = createStackNavigator({
     signin: {
@@ -32,7 +34,7 @@ const authStack = createStackNavigator({
     forgotpwd: {
         screen: ForgotPwdScreen,
         navigationOptions: {
-            headerTitle: null,
+            headerTitle: () => null,
             headerBackTitle: 'back',
             headerBackTitleStyle: {
                 fontWeight: 'bold',
@@ -44,18 +46,18 @@ const authStack = createStackNavigator({
         }
     },
 });
-/*
+
 const homeStack = createStackNavigator({
     home: {
         screen: HomeScreen,
     },
-}, {
+},{
     defaultNavigationOptions: ({navigation}) => ({
         headerStyle: {
             backgroundColor: COLORS.primary,
             shadowOpacity: 0,
         },
-        headerTitle: 'Find Best Restaurents',
+        headerTitle: 'Best tienda de segunda',
         headerTitleStyle: {
             color: COLORS.white,
             fontWeight: '800',
@@ -68,20 +70,20 @@ const homeStack = createStackNavigator({
             />
         ),
     }),
-}) */
+}) 
 
-/* const bottomStack = createBottomTabNavigator({
+const bottomStack = createBottomTabNavigator({
     history: {
         screen: HistoryScreen,
         navigationOptions: {
-            tabBarLabel: 'History',
+            tabBarLabel: 'Historial',
             tabBarIcon: ({tintColor, focused}) => <Icon name='history' size={focused ? 30: 20} color={tintColor} />
         }
     },
     cart: {
         screen: CartScreen,
         navigationOptions: {
-            tabBarLabel: 'Cart',
+            tabBarLabel: 'Carrito',
             tabBarIcon: ({tintColor, focused}) => <Icon name='cart-outline' size={focused ? 30: 20} color={tintColor} />
         }
     },
@@ -95,17 +97,25 @@ const homeStack = createStackNavigator({
     favourite: {
         screen: FavouritesScreen,
         navigationOptions: {
-            tabBarLabel: 'Favourites',
+            tabBarLabel: 'Favoritos',
             tabBarIcon: ({tintColor, focused}) => <Icon name='heart-outline' size={focused ? 30: 20} color={tintColor} />
         }
     },
     account: {
         screen: AccountScreen,
         navigationOptions: {
-            tabBarLabel: 'Account',
+            tabBarLabel: 'Cuenta',
             tabBarIcon: ({tintColor, focused}) => <Icon name='account-outline' size={focused ? 30: 20} color={tintColor} />
         }
+    },
+    prevista: {
+        screen: PrevistaScreen,
+        navigationOptions: {
+            tabBarLabel: 'prevista',
+            tabBarIcon: ({tintColor, focused}) => <Icon name='camera-outline' size={focused ? 30: 20} color={tintColor} />
+        }
     }
+
 },{
     initialRouteName: 'home',
     tabBarOptions: {
@@ -114,10 +124,14 @@ const homeStack = createStackNavigator({
         labelStyle: {
             fontWeight: 'bold',
         },
+        tabStyle: {
+            paddingTop: 5,
+            height: 50,
+        }
     }
 });
 
-/*
+
 const drawerStack = createDrawerNavigator({
     drawer: {
         screen: bottomStack,
@@ -138,8 +152,8 @@ const mainStack = createSwitchNavigator({
     main : {
         screen: drawerStack,
     }
-})*/
+})
 
-const router = createAppContainer(authStack/*,bottomStack, mainStack*/); 
+const router = createAppContainer(mainStack); 
 
 export default router;
