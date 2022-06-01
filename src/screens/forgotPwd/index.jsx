0bb5d15d320645bcb65b53107ea68a91fe40fac7
Lynnen04 <55860738+DREESEL@users.noreply.github.com>
2022-05-31 19:50:22 -0500

    Update index.jsx

--- src/screens/forgotPwd/index.jsx
+++ src/screens/forgotPwd/index.jsx
@@ -21,17 +21,3 @@ const ForgotPwdScreen = () => {
 }
 
 export default ForgotPwdScreen;
-
-/* <View style={styles.container}>
-            <Text style={styles.title}>Forgot Password</Text>
-            <Text style={styles.subtitle}>Enter your email address below and we will send you an email with instructions on how to change your password.</Text>
-            <View>
-                <TextInput placeholder='Enter your email' style={styles.textinput} />
-            </View>
-            <TouchableOpacity onPress={() => {}}>
-                <View style={styles.button}>
-                    <Text style={styles.buttonTxt}>Send</Text>
-                </View>
-            </TouchableOpacity>
-        </View> */
-        
