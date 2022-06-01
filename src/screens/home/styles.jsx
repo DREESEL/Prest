25cbc789b9f500a4647622f2cea06cf008bc126d
Lynnen04 <55860738+DREESEL@users.noreply.github.com>
2022-05-31 19:52:22 -0500

    Update styles.jsx

--- src/screens/home/styles.jsx
+++ src/screens/home/styles.jsx
@@ -1,13 +1,14 @@
 import { StyleSheet } from "react-native";
-/*import { COLORS, SIZES } from "../../constants/theme";*/
+import { YellowBox } from "react-native-web";
+import { COLORS, SIZES } from "../../constants/theme";
 
 const styles = StyleSheet.create({
     container: {
         flex: 3,
         justifyContent: "center",
         alignItems: "center",
-    }
-    /*top: {
+    },
+    top: {
         backgroundColor: COLORS.primary,
         padding: 10,
         alignItems: 'center',
@@ -22,7 +23,7 @@ const styles = StyleSheet.create({
     search: {
         marginLeft: 10,
         color: COLORS.title,
-    }*/
+    },
 });
 
 export default styles;
