f309fa427b588c3ccbd14975ea62dae3f8d9d7e0
Lynnen04 <55860738+DREESEL@users.noreply.github.com>
2022-05-31 19:48:31 -0500

    Update styles.jsx

--- src/screens/account/styles.jsx
+++ src/screens/account/styles.jsx
@@ -1,11 +1,30 @@
-import { StyleSheet } from "react-native";
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import styles from './styles';
+import { auth } from '../firebase';
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
-});
+const AccountScreen = ({ navigation }) => {
 
-export default styles;
+    const handleSignOut = () => {
+        auth
+          .signOut()
+          .then(() =>  {
+            navigation.navigate('auth')
+          })
+          .catch(error => alert(error.message))
+      }
+      
+    return(
+        <View style={styles.container}>
+        <Text>Email: {auth.currentUser?.email}</Text>
+        <TouchableOpacity
+          onPress={handleSignOut}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Sign Out </Text>
+        </TouchableOpacity>
+      </View>
+    )
+}
+
+export default AccountScreen;
