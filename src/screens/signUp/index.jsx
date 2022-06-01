f381496ab4ded91089022d8bcfcbbc2770055741
Lynnen04 <55860738+DREESEL@users.noreply.github.com>
2022-05-31 19:53:54 -0500

    Update index.jsx

--- src/screens/signUp/index.jsx
+++ src/screens/signUp/index.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState }  from 'react';
 import { View, Text, ImageBackground, ScrollView, TextInput, TouchableOpacity, Image } from 'react-native';
 import styles from './styles';
 import { COLORS } from '../../constants/theme';
+import { auth } from '../firebase';
 
-const SignUpScreen = ( { navigation } ) => {
+
+const SignUpScreen = ({ navigation }) => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSignUp = () => {
+        auth 
+        .createUserWithEmailAndPassword(email,password)
+        .then(userCredentials => {
+            const user = userCredentials.user;
+            console.log('Registro de :',user.email);
+        })
+        .catch(error => alert(error.message))
+      }
+
     return( 
         <View style={styles.container}>
             <ImageBackground
@@ -19,12 +35,12 @@ const SignUpScreen = ( { navigation } ) => {
                         <Text style={styles.subtitle}>Registrate para continuar</Text>
                     </View>
                     <View style={styles.dataContainer}>
-                        <TextInput placeholder='Correo' style={styles.textinput} placeholderTextColor={COLORS.white} />
+                        <TextInput placeholder='Correo' value={email} onChangeText={text => setEmail(text)} style={styles.textinput} placeholderTextColor={COLORS.white} />
                         <TextInput placeholder='Teléfono' style={styles.textinput} placeholderTextColor={COLORS.white} />
-                        <TextInput placeholder='Contraseña' style={styles.textinput} placeholderTextColor={COLORS.white} />
+                        <TextInput placeholder='Contraseña' value={password} onChangeText={text => setPassword(text)} style={styles.textinput} placeholderTextColor={COLORS.white}/>
                     </View>
                     <View style={styles.btnContainer}>
-                        <TouchableOpacity onPress={() => {}}>
+                        <TouchableOpacity onPress={handleSignUp}>
                             <View style={styles.button1}>
                                 <Text style={styles.btnText}>Registrate</Text>
                             </View>
@@ -56,50 +72,3 @@ const SignUpScreen = ( { navigation } ) => {
 }
 
 export default SignUpScreen;
-/* <View style={styles.container}>
-            <ImageBackground
-                source={require('../../assets/background.png')}
-                style={{
-                    flex: 1
-                }}
-                resizeMode= "cover"
-            >
-                <ScrollView>
-                    <View style={styles.topContainer}>
-                        <Text style={styles.title}>Get Started</Text>
-                        <Text style={styles.subtitle}>Sign up to continue</Text>
-                    </View>
-                    <View style={styles.dataContainer}>
-                        <TextInput placeholder='Email' style={styles.textinput} placeholderTextColor={COLORS.white} />
-                        <TextInput placeholder='Contact Number' style={styles.textinput} placeholderTextColor={COLORS.white} />
-                        <TextInput placeholder='Password' style={styles.textinput} placeholderTextColor={COLORS.white} />
-                    </View>
-                    <View style={styles.btnContainer}>
-                        <TouchableOpacity onPress={() => {}}>
-                            <View style={styles.button1}>
-                                <Text style={styles.btnText}>SIGN UP</Text>
-                            </View>
-                        </TouchableOpacity>
-                        <TouchableOpacity onPress={() => {}}>
-                            <View style={styles.button2}>
-                                <View style={styles.logo}>
-                                    <Image source={require('../../assets/facebook.png')} 
-                                        resizeMode="contain"   
-                                        style={{
-                                            width: 30,
-                                            height: 30,
-                                        }}
-                                    />
-                                </View>
-                                <Text style={styles.btnText}>Sign In with facebook</Text>
-                            </View>
-                        </TouchableOpacity>
-                    </View>
-                    <View style={styles.bottomContainer}>
-                        <TouchableOpacity onPress={() => navigation.navigate('signin')}>
-                            <Text style={styles.text}>Already have an account? | Sign In</Text>
-                        </TouchableOpacity>
-                    </View>
-                </ScrollView>
-            </ImageBackground>
-        </View> */
