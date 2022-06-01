037c64d88d2d365aa0167b90460e888c34852353
Lynnen04 <55860738+DREESEL@users.noreply.github.com>
2022-05-31 19:48:57 -0500

    Update index.jsx

--- src/screens/account/index.jsx
+++ src/screens/account/index.jsx
@@ -1,12 +1,29 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import styles from './styles';
+import { auth } from '../firebase';
 
-const AccountScreen = () => {
+const AccountScreen = ({ navigation }) => {
+
+    const handleSignOut = () => {
+        auth
+          .signOut()
+          .then(() =>  {
+            navigation.navigate('auth')
+          })
+          .catch(error => alert(error.message))
+      }
+      
     return(
         <View style={styles.container}>
-            <Text>Account Screen</Text>
-        </View>
+        <Text>Email: {auth.currentUser?.email}</Text>
+        <TouchableOpacity
+          onPress={handleSignOut}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Sign Out </Text>
+        </TouchableOpacity>
+      </View>
     )
 }
 
