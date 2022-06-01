61501d6169349d33c2a146916116881ff92abc39
Lynnen04 <55860738+DREESEL@users.noreply.github.com>
2022-05-31 19:51:49 -0500

    Update index.jsx

--- src/screens/home/index.jsx
+++ src/screens/home/index.jsx
@@ -1,38 +1,31 @@
 import React from "react";
-import { View, Text/*, TextInput, ScrollView */} from "react-native";
+import { View, Text, TextInput, ScrollView } from "react-native";
 import styles from './styles';
-/*import Icon from 'react-native-vector-icons/FontAwesome';*/
-/*import { COLORS } from '../../constants/theme';*/
-/*import Restaurent from "../../components/restaurent";*/
-/*import DATA from "../../data/data.json";*/
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { COLORS } from '../../constants/theme';
+import Galeriacontenido from "../../components/galeriacontenido";
+import DATA from "../../data/data.json";
 
 const HomeScreen = () => {
-
+    const filterData = (price) => (DATA.galeriacontenidos).filter((result) => result.price === price );
     // filtro por tipo de precio
     return (
-        <View style={styles.container}>
-            <Text>Home screen</Text>
-        </View>
-    )
-        
-}
-
-export default HomeScreen;
-
-/*const filterData = (price) => (DATA.restaurents).filter((result) => result.price === price );
-    return(
         <View>
             <View style={styles.top}>
                 <View style={styles.searchBox}>
                     <Icon name="search" size={25} color={COLORS.grey} />
-                    <TextInput placeholder="what you like to eat?" style={styles.search} />
+                    <TextInput name="txtspace" placeholder="Qué deseas?" style={styles.search} />
                 </View>
             </View>
             <ScrollView>
-                <Restaurent title="Cost Effective" data={filterData('$')} />
-                <Restaurent title="Bit Pricer" data={filterData('$$')} />
-                <Restaurent title="Big Spender" data={filterData('$$$')} />
+                <Galeriacontenido title="Nuevos" data={filterData('$')} />
+                <Galeriacontenido title="Recomendaciones" data={filterData('$$')} />
+                <Galeriacontenido title="Populares" data={filterData('$$$')} />
                 <View style={{ height: 80 }}/>
             </ScrollView>
         </View>
-    )*/
+    )   
+}
+
+export default HomeScreen;
+
