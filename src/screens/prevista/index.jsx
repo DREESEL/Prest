4eeba3ca86ce97dba92e9b0eb61fd757c1ad683b
Toshico <colcapedro@gmail.com>
2022-06-02 15:53:31 -0500

    Pequeños cambios

--- src/screens/prevista/index.jsx
+++ src/screens/prevista/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text } from "react-native";
 import styles from './styles';
 
+
 const PrevistaScreen = () => {
     return(
         <View style={styles.container}>
