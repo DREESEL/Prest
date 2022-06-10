a585c271e4edf1eb74977eccf0960db47be257f3
Lynnen04 <55860738+DREESEL@users.noreply.github.com>
2022-06-10 00:59:00 -0500

    Update styles.jsx

--- src/screens/prevista/styles.jsx
+++ src/screens/prevista/styles.jsx
@@ -2,10 +2,15 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+      flex: 1,
+      backgroundColor: '#000',
+      justifyContent: 'center',
+      paddingBottom: 20 
+    },
+    camera: {
+      flex: 1,
+      borderRadius: 20,
     }
-});
+}); 
 
 export default styles;
